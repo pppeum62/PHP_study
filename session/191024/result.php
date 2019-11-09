0cb6599b991c0203c27dd0489a0f84caf2a62ea1
pppeum <s2018s13@e-mirim.hs.kr>
2019-11-09 19:28:14 +0900

    191024

---
+++ session/191024/result.php
@@ -0,0 +1,86 @@
+<?php
+    $name = $_POST{'name'};
+    $id = $_POST{'id'};
+    $pw = $_POST{'pw'};
+    $email = $_POST{'email'};
+    $r1 = $_POST{'birth'};
+    $r2 = $_POST{'gender'};
+    $birth = $_POST{'birth'};
+    $gender = $_POST{'gender'};
+    $tel_first = $_POST{'tel-first'};
+    $tel_mid = $_POST{'tel-mid'};
+    $tel_last = $_POST{'tel-last'};
+    $tel = "$tel_first"."-"."$tel_mid"."-"."$tel_last";
+    $address = $_POST{'address'};
+
+    $yr = substr($birth, 0, 2);
+
+    if ($r2 == "1" || $r2 == "2") {
+        $year = 1900 + $yr;
+    } else {
+        $year = 2000 + $yr;
+    }
+
+    $age = date("Y", time()) - $year + 1;
+
+    if ($r1 == "1" || $r1 == "3") {
+        $gender = "M";
+    } else if ($r1 == "2" || $r1 == "4") {
+        $gender = "F";
+    }
+
+    $conn = mysqli_connect('127.0.0.1', 'root', 'mirim2', 'php');
+
+    $data_stream = " '$name', '$pw', '$birth', '$tel', $age ";
+    $sql = "insert into members (name, password, birth, tel, age) values(".$data_stream.")";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "insert 성공";
+    } else {
+        echo "실패 : " . mysqli_error($conn);
+    }
+
+    echo "<table>";
+
+    echo "<tr>";
+    echo "<td>이름</td>";
+    echo "<td>"."$name"."</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+    echo "<td>아이디</td>";
+    echo "<td>"."$id"."</td";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>이메일</td>";
+    echo "<td>".$email."</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>나이</td>";
+    echo "<td>".$age."</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>성별</td>";
+    if ($gender == "M") {
+        echo "<td>남자</td>";
+    } else {
+        echo "<td>여자</td>";
+    }
+
+    echo "<tr>";
+    echo "<td>전화번호</td>";
+    echo "<td>"."$tel"."</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>주소</td>";
+    echo "<td>"."$address"."</td>";
+    echo "</tr>";
+
+    echo "</table>";
+
+
+?>
