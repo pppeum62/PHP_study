4fcf4c05580b3f54e53f3018039b25d82cc07ca6
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2019-12-07 21:21:29 +0900

    check ID overlap
    회원가입 시 아이디 중복 확인 추가

--- session/191024/result.php
+++ session/191024/result.php
@@ -14,73 +14,85 @@
     $address = $_POST{'address'};
     $address_details = $_POST{'address_details'};
 
-    $yr = substr($birth, 0, 2);
-
-    if ($r2 == "1" || $r2 == "2") {
-        $year = 1900 + $yr;
-    } else {
-        $year = 2000 + $yr;
-    }
-
-    $age = date("Y", time()) - $year + 1;
-
-    if ($r2 == "1" || $r2 == "3") {
-        $gender = "남자";
-    } else if ($r2 == "2" || $r2 == "4") {
-        $gender = "여자";
-    }
-
     $conn = mysqli_connect('127.0.0.1', 'root', 'mirim2', 'php');
-
-    $data_stream = " '$id', '$name', '$pw', '$birth', '$address', '$address_details', '$tel', '$email', '$gender', $age ";
-    $sql = "insert into members (id, name, password, birth, addr1, addr2, tel, email, gender, age) values(".$data_stream.")";
-
-    if (mysqli_query($conn, $sql)) {
-        echo "insert 성공";
-    } else {
-        echo "실패 : " . mysqli_error($conn);
-    }
-
-    echo "<table>";
-
-    echo "<tr>";
-    echo "<td>이름</td>";
-    echo "<td>"."$name"."</td>";
-    echo "</tr>";
     
-    echo "<tr>";
-    echo "<td>아이디</td>";
-    echo "<td>"."$id"."</td";
-    echo "</tr>";
+    /* 아이디 중복 확인 */
+    $sql = 'select * from members where id="'.$id.'"';
+    $result = mysqli_query($conn, $sql);
 
-    echo "<tr>";
-    echo "<td>이메일</td>";
-    echo "<td>".$email."</td>";
-    echo "</tr>";
+    if(mysqli_num_rows($result) > 0) {      // 아이디가 중복 되었다면 alert 후 이전 페이지(회원가입 화면)로
+        echo '<script>alert("중복된 아이디가 있습니다. 다른 아이디를 입력해주세요."); history.go(-1);</script>';
+    }
+    /* 아이디 중복 확인 */
 
-    echo "<tr>";
-    echo "<td>나이</td>";
-    echo "<td>".$age."</td>";
-    echo "</tr>";
+    else {      // 새로운 아이디라면 실행
+        $yr = substr($birth, 0, 2);
 
-    echo "<tr>";
-    echo "<td>성별</td>";
-    if ($gender == "남자") {
-        echo "<td>남자</td>";
-    } else {
-        echo "<td>여자</td>";
+        if ($r2 == "1" || $r2 == "2") {
+            $year = 1900 + $yr;
+        } else {
+            $year = 2000 + $yr;
+        }
+
+        $age = date("Y", time()) - $year + 1;
+
+        if ($r2 == "1" || $r2 == "3") {
+            $gender = "남자";
+        } else if ($r2 == "2" || $r2 == "4") {
+            $gender = "여자";
+        }
+
+        $data_stream = " '$id', '$name', '$pw', '$birth', '$address', '$address_details', '$tel', '$email', '$gender', $age ";
+        $sql = "insert into members (id, name, password, birth, addr1, addr2, tel, email, gender, age) values(".$data_stream.")";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "insert 성공";
+        } else {
+            echo "실패 : " . mysqli_error($conn);
+        }
+
+        echo "<table>";
+
+        echo "<tr>";
+        echo "<td>이름</td>";
+        echo "<td>"."$name"."</td>";
+        echo "</tr>";
+        
+        echo "<tr>";
+        echo "<td>아이디</td>";
+        echo "<td>"."$id"."</td";
+        echo "</tr>";
+
+        echo "<tr>";
+        echo "<td>이메일</td>";
+        echo "<td>".$email."</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+        echo "<td>나이</td>";
+        echo "<td>".$age."</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+        echo "<td>성별</td>";
+        if ($gender == "남자") {
+            echo "<td>남자</td>";
+        } else {
+            echo "<td>여자</td>";
+        }
+
+        echo "<tr>";
+        echo "<td>전화번호</td>";
+        echo "<td>"."$tel"."</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+        echo "<td>주소</td>";
+        echo "<td>"."$address"."</td>";
+        echo "</tr>";
+
+        echo "</table>";
     }
 
-    echo "<tr>";
-    echo "<td>전화번호</td>";
-    echo "<td>"."$tel"."</td>";
-    echo "</tr>";
-
-    echo "<tr>";
-    echo "<td>주소</td>";
-    echo "<td>"."$address"."</td>";
-    echo "</tr>";
-
-    echo "</table>";
-
+    mysqli_close($conn);
 ?>
